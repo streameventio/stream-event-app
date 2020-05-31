4afc658b8e7d54d192f3c1dd5faacfda7c4f3544
Jingles <jingles@Jingless-MacBook-Pro.local>
2020-05-31 22:20:57 +0800

    update

---
+++ src/components/App/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import Materialize from 'materialize-css';
+
+// import './css/materialize.css';
+// import './css/style.css';
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn Reactssaa
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// class PageNotFound extends React.Component {
+//   render() {
+//     return <h1>PageNotFound!!</h1>
+//   }
+// }
+// export default PageNotFound
+
+import React from 'react'
+const PageNotFound = () => <h1>Not found</h1>
+export default PageNotFound
