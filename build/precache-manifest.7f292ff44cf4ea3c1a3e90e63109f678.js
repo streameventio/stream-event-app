b479b5c2fe8933943ddd07f45263e782c2599663
Jingles <jingles@Jingless-MacBook-Pro.local>
2020-06-01 10:58:48 +0800

    up

---
+++ build/precache-manifest.7f292ff44cf4ea3c1a3e90e63109f678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647fbce1cbdd8599717fe37bc95f0c77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60561a2b3ebcca938238",
+    "url": "/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "60561a2b3ebcca938238",
+    "url": "/static/js/2.18c41889.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.18c41889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2431c3d68dd11d5c4774",
+    "url": "/static/js/main.c2cf11c2.chunk.js"
+  },
+  {
+    "revision": "6d2822a29c2647d519b0",
+    "url": "/static/js/runtime-main.48e84fc6.js"
+  }
+]);
